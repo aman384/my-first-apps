f6a36c2ac93f18ece19d383f860af353f5ab446f
Divyansh Ingle <divyansh@espsofttechnologies.com>
2021-01-12 18:40:10 +0530

    first free

--- src/App.js
+++ src/App.js
@@ -1,38 +1,111 @@
 import React from 'react';
+// import logo from './logo.svg';
+import './assets/jquery.emojiarea.css'
+// import './assets/stylesheet.css'
+import './assets/fontello.css'
 import './App.css';
-import Dashboard from './component/dashboard'
-import Profile from './component/profile'
-import Page3 from './component/page3'
+// import './assets/css/bootstrap.css';
+import './assets/css/font-awesome.min.css';
+import './assets/plugins/owl/owl.carousel.css'
+import './assets/plugins/owl/owl.theme.css'
+import './assets/plugins/owl/owl.transitions.css'
+import './assets/plugins/aos-master/aos.css'
+import './assets/css/responsive.css'
+import './assets/css/main.min.css'
+import './assets/css/style.css'
+import './assets/css/style1.css'
+import './assets/css/canvas.css'
+import './assets/themify-icons.css'
+import './assets/css/color.css'
+
+// import '~video-react/dist/video-react.css'; // import css
+
+import Home from './components/Home';
+import Header from './directives/header'
+import Footer from './directives/footer'
+import Dashboard from './components/Dashboard'
+import InnerHeader from './directives/innerHeader'
+import InnerSidebar from './directives/innerSidebar'
+import RightSidebar from './directives/rightSidebar'
+import Fixedsidebarright from './directives/Fixedsidebarright'
+import Timeline from './components/Timeline'
+import Wallet from './components/Wallet'
+import Creategroup from './components/Creategroup'
+import Forgetpassword from './components/Forgetpassword'
+import Resetpassword from './components/Resetpassword'
+import Groupdetail from './components/Groupdetail'
+import AnalyticsTraffic from './components/Analytics'
+import DiscoveryGroups from './components/Discoverygroup'
+import Setting from './components/Setting'
+import Messaging from './components/messaging'
+import Twofa from './components/Twofa'
+import Headerhome from './directives/headerhome'
+import Overview from './components/Overview'
+import Createproject from './components/CreateProject'
+import Allprojects from './components/Allproject'
+import Profilesetup from './components/Profilesetup'
+import Pro from './components/Pro'
+import Plus from './components/Plus'
+import Allprojectshome from './components/Allprojecthome'
+import MyGroups from './components/myGroups'
+import MyProjects from './components/Myprojects'
+import DiscoveryProjects from './components/DiscoverProject'
+import AllNotifications from './components/Allnotification'
+import Home1 from './components/Home1'
+import BuyEther from './components/BuyEth'
 
 
-// import './assets/js/bootstrap.js';
+
 import { HashRouter, Route, Switch } from 'react-router-dom'
 
-function App() {
-  // var myStyle = {
-  //   fontSize: 100,
-  //   color: '#FF0000'
-  // }
 
+function App() {
   return (
+
     <HashRouter>
       <div>
-        {/* <Menu /> */}
         <Switch>
+        {/* <Route path="/freedom-cells-react/" exact component={Home} /> */}
+          
+          <Route path="/freedom-cells-react/" exact component={Home} />
+          <Route path="/freedom-cells-react/Login=:id" component={Home1} />
 
-          {/* red-block */}
-          <Route exact path="/" component={Dashboard} />
-          <Route path="/profile" component={Profile} />
-          <Route path="/page3" component={Page3} />
-
-
-
+          <Route path="/freedom-cells-react/header" component={Header} />
+          <Route path="/freedom-cells-react/footer" component={Footer} />
+          <Route path="/freedom-cells-react/dashboard" component={Dashboard} />
+          <Route path="/freedom-cells-react/innerHeader" component={InnerHeader} />
+          <Route path="/freedom-cells-react/innerSidebar" component={InnerSidebar} />
+          <Route path="/freedom-cells-react/rightSidebar" component={RightSidebar} />
+          <Route path="/freedom-cells-react/timeLine/:id/:post_id" component={Timeline} />
+          <Route path="/freedom-cells-react/timeLine/:id" component={Timeline} />
+          <Route path="/freedom-cells-react/creategroup" component={Creategroup} />
+          <Route path="/freedom-cells-react/fixedsidebarright" component={Fixedsidebarright} />
+          <Route path="/freedom-cells-react/wallet" component={Wallet} />
+          <Route path="/freedom-cells-react/analyticstraffic" component={AnalyticsTraffic} />
+          <Route path="/freedom-cells-react/discoverygroups" component={DiscoveryGroups} />
+          <Route path="/freedom-cells-react/forgetpassword" component={Forgetpassword} />
+          <Route path="/freedom-cells-react/setting" component={Setting} />
+          <Route path="/freedom-cells-react/messaging" component={Messaging} />
+          <Route path="/freedom-cells-react/twofa" component={Twofa} />
+          <Route path="/freedom-cells-react/resetpassword/:code" component={Resetpassword} />
+          <Route path="/freedom-cells-react/groupdetail/:group_id" component={Groupdetail} />
+          <Route path="/freedom-cells-react/Headerhome" component={Headerhome} />
+          <Route path="/freedom-cells-react/overview" component={Overview} />
+          <Route path="/freedom-cells-react/createproject" component={Createproject} />
+          <Route path="/freedom-cells-react/allprojects" component={Allprojects} />
+          <Route path="/freedom-cells-react/profilesetup" component={Profilesetup} />
+          <Route path="/freedom-cells-react/pro" component={Pro} />
+          <Route path="/freedom-cells-react/plus" component={Plus} />
+          <Route path="/freedom-cells-react/allprojectslist" component={Allprojectshome} />
+          <Route path="/freedom-cells-react/MyGroup" component={MyGroups} />
+          <Route path="/freedom-cells-react/MyProject" component={MyProjects} />
+          <Route path="/freedom-cells-react/DiscoveryProject" component={DiscoveryProjects} />
+          <Route path="/freedom-cells-react/AllNotification" component={AllNotifications} />
+          <Route path="/freedom-cells-react/BuyEth" component={BuyEther} />
         </Switch>
       </div>
     </HashRouter>
-
   );
 }
 
 export default App;
-
---
+++ src/directives/innerSidebar.js
@@ -0,0 +1,164 @@
+import React, { Component } from 'react';
+import Cookies from 'js-cookie';
+import { Link } from 'react-router-dom';
+import axios from 'axios';
+import $ from 'jquery'
+
+export default class InnerSidebar extends Component {
+
+    constructor(props) {
+        super(props)
+        let data = Cookies.getJSON('name');
+        this.loginData = data.user_data
+        this.state = {
+            listGroup: []
+        }
+    }
+
+    componentDidMount() {
+        this.groupAPI()
+        this.showDisplayNameAPI()
+        var urlGroup = window.location.href.split('groupdetail/')[1]
+        this.setState({
+            urlGroup: urlGroup
+        })
+        $(".sidebar-dropdown > a").click(function () {
+            $(".sidebar-submenu").slideUp(250);
+            if (
+                $(this)
+                    .parent()
+                    .hasClass("active")
+            ) {
+                $(".sidebar-dropdown").removeClass("active");
+                $(this)
+                    .parent()
+                    .removeClass("active");
+            } else {
+                $(".sidebar-dropdown").removeClass("active");
+                $(this)
+                    .next(".sidebar-submenu")
+                    .slideDown(250);
+                $(this)
+                    .parent()
+                    .addClass("active");
+            }
+        });
+
+        $("#toggle-sidebar").click(function () {
+            $(".page-wrapper").toggleClass("toggled");
+        });
+    }
+
+    //==================================  Detail of Group List  ==============================
+
+    groupAPI() {
+        $('#main_loader').show();
+        $('#root').css('opacity', '0.5');
+        axios.post(`/freedomcell/api/users/group_list`, { 'user_id': this.loginData.id, 'view_user_id': this.loginData.id, 'api_key': this.loginData.api_key }).then((res) => {
+            this.codeDataGroup = res.data.code
+            if (this.codeDataGroup === true) {
+                this.setState({
+                    listGroup: res.data.recdata
+                });
+            }
+            $('#main_loader').hide();
+            $('#root').css('opacity', '1.0');
+        }).catch((error) => {
+        });
+    }
+
+    loading(id) {
+        $('#main_loader').show();
+        $('#root').css('opacity', '0.5');
+        setTimeout(() => {
+            window.location.href = '/freedom-cells-react/timeLine/' + id;
+            $('#main_loader').hide();
+            $('#root').css('opacity', '1.0');
+        }, 200);
+    }
+
+    groupDetail(id) {
+        $('#main_loader').show();
+        $('#root').css('opacity', '0.5');
+        setTimeout(() => {
+            window.location.href = '/freedom-cells-react/timeLine/' + id;
+            $('#main_loader').hide();
+            $('#root').css('opacity', '1.0');
+        }, 200);
+    }
+
+    //=====================================   Show Display NAme    ====================================
+
+    showDisplayNameAPI() {
+        $('#main_loader').show();
+        $('#root').css('opacity', '0.5');
+        axios.post(`/freedomcell/api/users/show_display_name`, { 'user_id': this.loginData.id, 'api_key': this.loginData.api_key }).then((res) => {
+            this.codeDataDisplayName = res.data.code
+            if (this.codeDataDisplayName === true) {
+                this.setState({
+                    list: res.data
+                });
+            }
+            $('#main_loader').hide();
+            $('#root').css('opacity', '1.0');
+        }).catch((error) => {
+        });
+    }
+
+    render() {
+        return (
+            <div className="col-lg-3">
+                <aside className="sidebar fixed">
+                    <div className="widget" style={{ height: '352px', overflowY: 'auto', width: '307px', marginLeft: '-10px' }}>
+                        <br />
+                        <ul className="naves">
+                            <li id="li_self">
+                                <i className="ti-user"></i>
+                                <Link to={`/freedom-cells-react/timeLine/${this.loginData?.id}`} onClick={this.loading.bind(this, this.loginData?.id)}>{this.state.list?.display_name} </Link>
+                            </li>
+                            <li id="li_dashboard">
+                                <i className="fa fa-home" aria-hidden="true"></i>
+                                <Link to="/freedom-cells-react/dashboard" > Home </Link>
+                            </li>
+                            <li id="li_wallet">
+                                <i className="ti-wallet"></i>
+                                <Link to="/freedom-cells-react/wallet" title="" > Wallet </Link>
+                            </li>
+                            <li className="sidebar-dropdown">
+                                <a href="#" className="group_arrow" style={{ marginLeft: '23px' }}> <img style={{ color: '#999', marginLeft: '-28px' }} src="https://espsofttechnologies.com/freedom-cells-react/Projects_icon_small.png" /><span>Projects</span></a>
+                                <div className="sidebar-submenu" style={{ display: 'none' }}>
+                                    <ul>
+                                        <li id="li_projects">
+                                            <Link to="/freedom-cells-react/createproject" title="" >  Create New Project </Link>
+                                        </li>
+                                        <li id="li_my_projects"><Link to="/freedom-cells-react/MyProject" ><span>My Projects</span> </Link></li>
+                                        <li id="li_discovery_projects"><Link to="/freedom-cells-react/DiscoveryProject" ><span>Discover Projects</span> </Link></li>
+                                    </ul>
+                                </div>
+                            </li>
+                            <li className="sidebar-dropdown">
+                                <a href="#" className="group_arrow" style={{ marginLeft: '23px' }}>  <i className="fa fa-users" style={{ color: '#999', marginLeft: '-42px' }} aria-hidden="true"></i><span>Groups</span></a>
+                                <div className="sidebar-submenu" style={{ display: 'none' }}>
+                                    <ul>
+                                        <li id="li_creategroup"><Link to="/freedom-cells-react/creategroup" >
+                                            <span>Create Group</span> </Link></li>
+                                        <li id="li_my_groups"><Link to="/freedom-cells-react/MyGroup" ><span>My Groups</span> </Link></li>
+                                        <li id="li_discovery_groups"><Link to="/freedom-cells-react/discoverygroups" ><span>Discover Group</span> </Link></li>
+                                    </ul>
+                                </div>
+                            </li>
+                            <li id="li_analytics_traffic">
+                                <i className="fa fa-bar-chart-o"></i>
+                                <Link to="/freedom-cells-react/analyticstraffic" title=""> Analytics </Link>
+                            </li>
+                            <li id="li_settings">
+                                <i className="fa fa-gear"></i>
+                                <Link to="/freedom-cells-react/setting" title=""> Settings  </Link>
+                            </li>
+                        </ul>
+                    </div>
+                </aside>
+            </div>
+        )
+    }
+}
